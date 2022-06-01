816a18ec601b8f03aae42bc7a90a93a64f639f82
Juan Carlos <4522@holbertonschool.com>
2022-06-01 09:17:29 -0500

    MVC

--- app.js
+++ app.js
@@ -2,25 +2,22 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const hb = require('express-handlebars')
+const errorController = require('./controllers/error')
 
 const app = express()
 
-app.engine('hbs', hb({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'})
-})
+app.use(errorController.get404)
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,16 @@
 const path = require('path')
 const express = require('express')
 
-const rootDir = require('../util/path')
+const adminController = require('../controllers/admin')
 
 const router = express.Router()
 
-const products = []
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product', 
-        formsCSS: true, 
-        productCSS: true, 
-        activeAddProduct: true
-    })
-})
+router.get('/add-product', adminController.getAddProduct )
 
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title})
-    res.redirect('/')
-})
+router.get('/products',adminController.getProducts)
 
-exports.routes = router
-exports.products = products
+router.post('/add-product', adminController.postAddProduct)
+
+module.exports = router
 
