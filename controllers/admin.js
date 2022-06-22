2a3a3d206a16c1ea679635e2c2b4eaba5b03ebe0
Juan Carlos <4522@holbertonschool.com>
2022-06-22 13:18:30 -0500

    stripe payment

--- controllers/admin.js
+++ controllers/admin.js
@@ -163,8 +163,8 @@ exports.getProducts = (req, res, next) => {
       return next(error);});
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
   .then(product => {
     if(!product) {
@@ -175,9 +175,8 @@ exports.postDeleteProduct = (req, res, next) => {
   })
   .then(() => {
       console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+      res.status(200).json({message: 'succes'})
     })
-    .catch(err => {const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);});
-};
+    .catch(err => 
+     res.status(500).json({message: 'failed'})
+)};
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
+const session = require('express-session');
 const fs = require('fs');
 const path = require('path')
+const stripe = require('stripe')('sk_test_51D7qrmLXRn951fsahJZLaQGBNsnnf9Bhg5nQySBgQW0pfo8l5SLNju6WCcylGEBK4v4SHpScQEBZ65XQ8Kdzq5Ay00TCkYAkKP')
 
 const PDFDocument = require('pdfkit');
 const { isTypedArray } = require('util/types');
@@ -166,6 +168,75 @@ exports.postOrder = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);});
 };
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    //.execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.email,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch(err => {const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);});
+};
+
+exports.getCheckout = (req, res, next) => {
+  let total = 0
+  let products 
+  req.user
+    .populate('cart.items.productId')
+    //.execPopulate()
+    .then(user => {
+      products = user.cart.items;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price
+      })
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map(p => {
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: 'usd',
+            quantity: p.quantity
+          }
+        }),
+            success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+            cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+      })
+    })
+    .then( session => {
+      res.render('shop/checkout', {
+      path: '/checkout',
+      pageTitle: 'Checkout',
+      products: products,
+      totalSum: total,
+      sessionId: session.id
+     })
+    })
+     .catch(err => {const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);});
+}
 
 exports.getOrders = (req, res, next) => {
   Order.find({ 'user.userId': req.user._id })
